078215091751d5d911bb50500c2dca36db6029c3
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-02-17 10:19:25 +0100

    fix index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>comicsssssss</h1> -->
+@extends('layouts.app')
 
-     @foreach ($comics as $comic)
+@section('main_content')
+
+    @foreach ($comics as $comic)
         <div>
-            <h2>{{ $comics->title }}</h2>
-            <div>Tipo: {{ $comics->type }}</div>
+            <h2>{{ $comic->title }}</h2>
+            <div>Tipo: {{ $comic->type }}</div>
         </div>
     @endforeach
-</body>
-</html>
+@endsection
